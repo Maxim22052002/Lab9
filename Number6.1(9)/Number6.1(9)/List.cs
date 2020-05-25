6b86cfd2e7afe6289d82405008c79125cc885dde
Maxim22052002 <56323419+Maxim22052002@users.noreply.github.com>
2020-05-26 02:29:53 +0300

    Task6.1

---
+++ Number6.1(9)/Number6.1(9)/List.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Number6._1_9_
+{
+	public class ListNode<T>
+	{
+		public ListNode(T data) { Data = data; }
+		public T Data { get; set; }
+		public ListNode<T> Next { get; set; }
+	}
+	public class List<T> : IEnumerable<T>
+	{
+		ListNode<T> head, tail;
+		int counter;
+		public void Add(T data)
+		{
+			ListNode<T> node = new ListNode<T>(data);
+			if (head == null)
+				head = node;
+			else
+				tail.Next = node;
+			tail = node;
+			counter++;
+		}
+		public bool ContainsElem(T data)
+		{
+			ListNode<T> currentelem = head;
+			while (currentelem != null)
+			{
+				if (currentelem.Data.Equals(data))
+					return true;
+				currentelem = currentelem.Next;
+			}
+			return false;
+		}
+		public int Counter { get { return counter; } }
+		IEnumerator IEnumerable.GetEnumerator() { return ((IEnumerable)this).GetEnumerator(); }
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			ListNode<T> currentelem = head;
+			while (currentelem != null)
+			{
+				yield return currentelem.Data;
+				currentelem = currentelem.Next;
+			}
+		}
+	}
+}
---
+++ Number6.1(9)/Number6.1(9)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Number6._1_9_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = new List<int>()  { 7, 13, 21, 15 };
+            List<int> secondList = new List<int>() { 7, 21, 15, 13 }; 
+            int counter = 0;
+            foreach (int i in secondList)
+            {
+                if (firstList.ContainsElem(i))
+                {
+                    counter++;
+                }
+                    
+            }
+            if (counter == firstList.Counter)
+            {
+                Console.WriteLine("Первый список содержит ВСЕ элементы второго.");
+            }
+
+            else
+            {
+                Console.WriteLine("Первый список содержит НЕ ВСЕ элементы второго.");
+            }
+                
+        }
+    }
+}
