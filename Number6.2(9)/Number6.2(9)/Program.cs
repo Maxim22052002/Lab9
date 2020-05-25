d72cd2b855490acdc312e05f36b40e78bd280124
Maxim22052002 <56323419+Maxim22052002@users.noreply.github.com>
2020-05-26 02:31:03 +0300

    Task6.2

---
+++ Number6.2(9)/Number6.2(9)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Number6._2_9_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = new List<int>()  { 17, 22, 5, 59};
+            List<int> secondList = new List<int>() { 22, 17, 5, 59 }; 
+            int counter = 0;
+            foreach (int i in secondList)
+            {
+                if (firstList.Contains(i))
+                {
+                    counter++;
+                }
+                    
+            }
+            if (counter == firstList.Count)
+            {
+                Console.WriteLine("Первый список содержит ВСЕ элементы второго.");
+            }
+
+            else
+            {
+                Console.WriteLine("Первый список содержит НЕ ВСЕ элементы второго.");
+            }
+                
+        }
+    }
+}
