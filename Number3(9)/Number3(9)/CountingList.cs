d7b2b9feca79fd908d0cf2b4633483546a758db2
Maxim22052002 <56323419+Maxim22052002@users.noreply.github.com>
2020-05-26 02:23:14 +0300

    Task3

---
+++ Number3(9)/Number3(9)/CountingList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Number3_9_
+{
+    class ListNode
+    {
+        public string data;
+        public ListNode next;
+    }
+
+    public class CountingList
+    {
+        ListNode head = null;
+        ListNode tail = null;
+        public void Add(string FileNames)
+        {
+            ListNode node = new ListNode();
+            node.data = FileNames;
+            node.next = null;
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+                tail.next = head;
+            }
+            else
+            {
+                node.next = head;
+                tail.next = node;
+                tail = node;
+            }
+        }
+        public void Print()
+        {
+            ListNode node = head;
+            while (node != tail)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+            Console.WriteLine(tail.data);
+        }
+        public void Counting(string strname, int numberStr)
+        {
+            ListNode node = new ListNode();
+            node = head;
+            ListNode run;
+            while (node != tail)
+            {
+                if (node.data == strname)
+                {
+                    run = node;
+                    goto adder;
+                }
+                else
+                {
+                    node = node.next;
+                }
+            }
+            if (tail.data == strname)
+            {
+                run = tail;
+            }
+            else
+            {
+                Console.WriteLine("Проверьте введеное имя. Такого человека нет среди участников.");
+                return;
+            }
+        adder:
+            for (int i = 1; i < numberStr; i++)
+            {
+                run = run.next;
+                if (i == numberStr - 1)
+                {
+                    Console.WriteLine(run.data);
+                }
+            }
+        }
+    }
+}
---
+++ Number3(9)/Number3(9)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Number3_9_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст считалочки: ");
+            string inlet = Console.ReadLine();
+            Console.WriteLine("Введите имя человека, с которого начнется считалочка: ");
+            string name = Console.ReadLine();
+            string[] countingArray = CountingArray(inlet);
+            string[] names = NamesArray();
+            CountingList listnames = CountingListSet(names);
+            Console.WriteLine("В результате считалочки выходит:");
+            listnames.Counting(name, countingArray.Length);
+        }
+        static string[] NamesArray()
+        {
+            StreamReader read = File.OpenText(@"filenames.txt");
+            
+            string[] names = new string[10];
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = read.ReadLine();
+            }
+            read.Close();
+            return names;
+        }
+        static string[] CountingArray(string stringnames)
+        {
+            string[] array = stringnames.Split(new char[] { ' ' });
+            return array;
+        }
+        static CountingList CountingListSet(string[] MassiveNames)
+        {
+            CountingList listnames = new CountingList();
+            for (int i = 0; i < MassiveNames.Length; i++)
+            {
+                listnames.Add(MassiveNames[i]);
+            }
+            return listnames;
+        }
+    }
+}
