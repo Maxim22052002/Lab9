6a6cf00d2f54ae5165a28c9917e0a5ebdc081908
Maxim22052002 <56323419+Maxim22052002@users.noreply.github.com>
2020-05-26 02:21:56 +0300

    Task2

---
+++ Number2(9)/Number2(9)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Number2_9_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> parentheses = new Stack<char>();
+            Console.WriteLine("Введите выражение: ");
+            string MathExpression = Console.ReadLine();
+            try
+            {
+                for (int i = 0; i < MathExpression.Length; i++)
+                {
+                    if (MathExpression[i] == '(')
+                    {
+                        parentheses.Push(MathExpression[i]);
+                    }
+                    if (MathExpression[i] == ')')
+                    {
+                        parentheses.Pop();
+                    }
+                }
+                if (parentheses.Count == 0)
+                {
+                    Console.WriteLine("Выражение корректно.");
+                }
+                else
+                {
+                    Console.WriteLine("Выражение некорректно.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Выражение некорректно.");
+            }
+        }
+    }
+}
